1bb51a257c17aa0784888f7afcd7c4780d9fd23e
Krzysztof Wszoła <krzyzak864@gmail.com>
2025-10-28 02:45:24 +0100

    proba3

--- Wektor2D.hpp
+++ Wektor2D.hpp
@@ -1,45 +1,39 @@
 class Wektor2D
 {
-private:
-    void   setX(double X) { x = X; }
-    double getX() { return x; }
-    void   setY(double Y) { y = Y; }
-    double getY() { return y; }
-    /*
-            Wektor2D()
-            {
-                    double X=x;
-                    double Y=y;
-                    double norm = sqrt(X * X + Y * Y);
-                    void print(); { std::print("\nwektor: [{},{}]\t", x, y); }
-            }
-
-            ~Wektor2D()
-            {
-                    std::print("Wektor [{},{}] usuniety!\n", x, y);
-            }
-            */
-
-public:
     double x;
     double y;
 
-    Wektor2D() { std::print("W = [{},{}]\n", getX, getY); }
+public:
+    void   setX(double X) { x = X; };
+    double getX() { return x; };
+    void   setY(double Y) { y = Y; };
+    double getY() { return y; };
+
+    Wektor2D()
+    {
+        x = 0.;
+        y = 0.;
+    }
+    Wektor2D(double A, double B)
+    {
+        x = A;
+        y = B;
+    }
+    ~Wektor2D() {  }
 };
 
-/*
 Wektor2D operator+(Wektor2D A, Wektor2D B)
 {
-        Wektor2D C;
-        C.x = A.x + B.x;
-        C.y = A.y + B.y;
+    Wektor2D wynik;
 
-        return C;
+    wynik.setX(A.getX() + B.getX());
+    wynik.setY(A.getY() + B.getY());
+
+    return wynik;
 }
 
 double operator*(Wektor2D A, Wektor2D B)
 {
-        return A.x * B.x + A.y * B.y;
+    return A.getX() * B.getX() + A.getY() * B.getY();
 }
-*/
-    // Tutaj napisz implementacje klasy Wektor2D
+
